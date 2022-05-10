6072de0d813ea58c7578c54eb3aeb5d23c0e5a3f
33048岩崎要 <33048@ojs.ac.jp>
2022-05-10 14:54:42 +0900

    問題2.1.3完成

--- Chapter02/Exercise/Program.cs
+++ Chapter02/Exercise/Program.cs
@@ -7,6 +7,11 @@ using System.Threading.Tasks;
 namespace Exercise {
     class Program {
         static void Main(string[] args) {
+
+            Song[] songs = new Song[3];
+            songs[0] = new Song("夜に駆ける", "YOASOBI", 180);
+            songs[1] = new Song("もう少しだけ", "YOASOBI", 200);
+            songs[2] = new Song("怪物", "YOASOBI", 150);
         }
     }
 }
--- Chapter02/Exercise/Song.cs
+++ Chapter02/Exercise/Song.cs
@@ -6,14 +6,16 @@ using System.Threading.Tasks;
 
 namespace Exercise {
     class Song {
-        public string Title { get; set; }
-        public string ArtistName { get; set; }
-        public int Length { get; set; }
+        public string Title { get; set; } //歌のタイトル
+        public string ArtistName { get; set; } //アーティスト名
+        public int Length { get; set; } //演奏時間(秒)
 
-        public Song(string Title, string ArtistName, int Length) {
-            this.Title = Title;
-            this.ArtistName = ArtistName;
-            this.Length = Length;
+        public Song(string title, string artistName, int length) {
+            this.Title = title;
+            this.ArtistName = artistName;
+            this.Length = length;
         }
+
+        
     }
 }
