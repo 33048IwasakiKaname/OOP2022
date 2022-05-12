21b5a75b5d408fa8cb87a01f9c80a3f640fc178d
33048岩崎要 <33048@ojs.ac.jp>
2022-05-12 13:47:16 +0900

    問題2.2完成

---
+++ Chapter02/Exercise2/InchConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2 {
+    class InchConverter {
+        private const double ratio = 0.0254;
+
+        //インチからメートルへ
+        public static double FromInch(int inch) {
+            return inch * ratio;
+        }
+
+        //メートルからインチへ
+        public static double ToInch(double mater) {
+            return mater / ratio;
+        }
+    }
+}
