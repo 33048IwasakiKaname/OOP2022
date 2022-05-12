496ebc3c5fad3243ab7b7177f3ea91af667857a7
33048岩崎要 <33048@ojs.ac.jp>
2022-05-12 14:52:12 +0900

    問題2.3完成

---
+++ Chapter02/Exercise3/Exercise3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise3 {
+    class Program {
+        static void Main(string[] args) {
+
+            var sales = new SalesCounter("sales.csv");
+            
+            Console.WriteLine("売上高の表示選択");
+            Console.WriteLine("1.店舗別");
+            Console.WriteLine("2.商品カテゴリー別");
+            Console.Write(">");
+            int num = int.Parse(Console.ReadLine());
+
+            if(num == 1) {
+                var amountPerStore = sales.GetPerStoreSales();
+                foreach (var obj in amountPerStore) {
+                    Console.WriteLine("{0} {1}", obj.Key, obj.Value);
+                }
+            }else {
+                var GetPerCategorySales = sales.GetPerCategorySales();
+                foreach (var obj in GetPerCategorySales) {
+                    Console.WriteLine("{0} {1}", obj.Key, obj.Value);
+                }
+            }
+        }
+    }
+}
