9c39532f61a441c143af08a90cad657a2fac1acc
33048岩崎要 <33048@ojs.ac.jp>
2022-05-17 15:39:00 +0900

    問題3.2.1、3.2.2、3.2.3完成

---
+++ Chapter03/Exercise02/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    class Program {
+        static void Main(string[] args) {
+            var names = new List<string> {
+               "Tokyo", "New Delhi", "Bangkok", "London", "Paris", "Berlin", "Canberra", "Hong Kong",
+            };
+
+            Exercise2_1(names);
+            Console.WriteLine("--------------------");
+
+            Exercise2_2(names);
+            Console.WriteLine("--------------------");
+
+            Exercise2_3(names);
+            Console.WriteLine("--------------------");
+
+            Exercise2_4(names);
+            Console.WriteLine("--------------------");
+        }
+
+        private static void Exercise2_1(List<string> names) {
+            Console.Write("都市名を入力。空行で終了：");
+            do {
+                var line = Console.ReadLine(); //入力取り込み
+                if (string.IsNullOrEmpty(line))
+                    break;
+                
+             int index = names.FindIndex(s => s == line);
+             Console.WriteLine(index);
+                
+            } while (true);
+        }
+
+        private static void Exercise2_2(List<string> names) {
+            var count = names.Count(s => s.Contains("o"));
+            Console.WriteLine(count);
+        }
+
+        private static void Exercise2_3(List<string> names) {
+            var list = names.Where(s => s.Contains("o")).ToList();
+            foreach(var n in list) {
+                Console.WriteLine(n);
+            }
+        }
+
+        private static void Exercise2_4(List<string> names) {
+
+        }
+    }
+}
