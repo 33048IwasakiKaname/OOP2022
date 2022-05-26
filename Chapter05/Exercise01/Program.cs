4d80c5a145f25a89fc644cc950f1060ddbc93d70
33048岩崎要 <33048@ojs.ac.jp>
2022-05-26 16:00:00 +0900

    問題5.1、5.2完成

---
+++ Chapter05/Exercise01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise01 {
+    class Program {
+        static void Main(string[] args) {
+
+            string word1, word2;
+
+            Console.Write("1つ目：");
+            word1 = Console.ReadLine();
+
+            Console.Write("2つ目：");
+            word2 = Console.ReadLine();
+
+            if (String.Compare(word1,word2,ignoreCase:true) == 0) {
+                Console.WriteLine("一致しています");
+            } else {
+                Console.WriteLine("一致していません");
+            }
+        }
+    }
+}
---
+++ Chapter05/Exercise02/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise02 {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.Write("数値入力：");
+            int.TryParse(Console.ReadLine(), out var num);
+            Console.WriteLine("{0:N0}", num);
+        }
+    }
+}
