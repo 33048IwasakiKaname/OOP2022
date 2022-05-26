fae0de40ebb41055d742bdf451f4b6cec3d1bb0d
33048岩崎要 <33048@ojs.ac.jp>
2022-05-26 14:55:28 +0900

    4章問題完成

--- Chapter04/Exercise02/Program.cs
+++ Chapter04/Exercise02/Program.cs
@@ -11,10 +11,13 @@ namespace Exercise02 {
             //4.2.1
             var ymCollection = new YearMonth[] {
                 new YearMonth(1996,12),
-                new YearMonth(1997,7),
-                new YearMonth(2000,12),
+                new YearMonth(1997,4),
+                new YearMonth(2000,1),
                 new YearMonth(2001,5),
                 new YearMonth(2002,7),
+                new YearMonth(2012,6),
+                new YearMonth(2100,10),
+                new YearMonth(2800,11),
             };
             //4.2.2
             Exercise2_2(ymCollection);
@@ -34,9 +37,33 @@ namespace Exercise02 {
         }
 
         private static void Exercise2_6(YearMonth[] ymCollection) {
-            foreach (var ym in ymCollection.OrderByDescending(ym => ym.Year)) {
+            //①月で昇順に出力
+            foreach (var ym in ymCollection.OrderBy(ym => ym.Month)) {
                 Console.WriteLine(ym);
             }
+
+            //区切り
+            Console.WriteLine("--------------------");
+
+            //②偶数年のみ昇順に出力
+            foreach (var ym in ymCollection.Where(ym => ym.Year % 2 ==0).OrderBy(ym=>ym.Year)) {
+                Console.WriteLine(ym);
+            }
+
+            //区切り
+            Console.WriteLine("--------------------");
+
+            //③うるう年のみを出力
+            foreach (var ym in ymCollection.Where(ym=> DateTime.IsLeapYear(ym.Year))){
+                Console.WriteLine(ym);
+            }
+
+            //区切り
+            Console.WriteLine("--------------------");
+
+            //④すべての年から12月に一番近い月を出力
+            var data = ymCollection.Max(ym=>ym.Month);
+            Console.WriteLine(data);
         }
 
         //4.2.3
