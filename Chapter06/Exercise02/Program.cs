7f3783d602688428352169d9cc4b15347a351e0a
33048岩崎要 <33048@ojs.ac.jp>
2022-06-02 15:19:39 +0900

    問題6-2すべて完成

--- Chapter06/Exercise02/Program.cs
+++ Chapter06/Exercise02/Program.cs
@@ -40,32 +40,42 @@ namespace Exercise02 {
             Exercise2_7(books);
         }
 
-        private static void Exercise2_1(List<Book> books) {
-
+        private static void Exercise2_1(List<Book> books) {         
+            var book = books.FirstOrDefault(s => s.Title == "ワンダフル・C#ライフ");
+            if(book != null) {
+                Console.WriteLine("値段：{0}ページ数：{1}", book.Price, book.Pages);
+            }
         }
 
         private static void Exercise2_2(List<Book> books) {
-
+            var count = books.Count(s=>s.Title.Contains("C#"));
+            Console.WriteLine(count+"冊");
         }
 
         private static void Exercise2_3(List<Book> books) {
-
+            var avePage = books.Where(s=>s.Title.Contains("C#")).Average(s=>s.Pages);
+            Console.WriteLine("平均：{0}ページ",avePage);
         }
 
         private static void Exercise2_4(List<Book> books) {
-
+            var book = books.FirstOrDefault(s=>s.Price>=4000);
+            Console.WriteLine(book.Title);
         }
 
         private static void Exercise2_5(List<Book> books) {
-
+            var book = books.Where(s=>s.Price<4000).Max(s=>s.Pages);
         }
 
         private static void Exercise2_6(List<Book> books) {
-
+            foreach (var book in books.Where(s=>s.Pages>=400).OrderByDescending(s=>s.Price)) {
+                Console.WriteLine("タイトル：{0}　価格：{1}",book.Title,book.Price);
+            }
         }
 
         private static void Exercise2_7(List<Book> books) {
-
+            foreach (var book in books.Where(s=>s.Title.Contains("C#")).Where(s=>s.Pages<=500).Select(s=>s.Title)) {
+                Console.WriteLine(book);
+            }
         }
 
 
