89c57a1c940e41e88f5357c41fbce52dc517d692
33048岩崎要 <33048@ojs.ac.jp>
2022-06-14 14:29:33 +0900

    途中経過

--- Test202206/TestProject/Test01/ScoreCounter.cs
+++ Test202206/TestProject/Test01/ScoreCounter.cs
@@ -7,33 +7,43 @@ namespace Test01 {
 
         // コンストラクタ
         public ScoreCounter(string filePath) {
-
-
-
-            
+            _score = ReadScore(filePath);
         }
 
 
         //メソッドの概要： 
         private static IEnumerable<Student> ReadScore(string filePath) {
-            
 
+            List<Student> sales = new List<Student>();
+            string[] lines = File.ReadAllLines(filePath);
+            foreach (string line in lines) {
+                string[] items = line.Split(',');
+                Student sale = new Student {
+                    Name = items[0],
+                    Subject = items[1],
+                    Score = int.Parse(items[2])
+                };
+                sales.Add(sale);
+            }
+            return sales;
 
-
-
-
-
-            
         }
 
         //メソッドの概要： 
         public IDictionary<string, int> GetPerStudentScore() {
 
+            var dict = new Dictionary<string, int>();
 
+            //科目別の点数を集計
+            foreach (var sale in _score) {
+                if (dict.ContainsKey(sale.Subject))
+                    dict[sale.Subject] += sale.Score;
+                else
+                    dict[sale.Subject] = sale.Score;
+            }
 
+            return dict;
 
-
-            
         }
     }
 }
