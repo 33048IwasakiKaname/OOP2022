177cfe12ead4cb170581bc3df0a821ca8a0dc2df
33048岩崎要 <33048@ojs.ac.jp>
2022-06-28 13:35:13 +0900

    XmlSample タイトル取得

---
+++ Chapter11/XmlSample/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using System.Net;
+
+namespace XmlSample {
+    class Program {
+        static void Main(string[] args) {
+            using (var wc = new WebClient()) {
+                var stream = wc.OpenRead("https://news.yahoo.co.jp/rss/topics/it.xml");
+
+                var xdoc = XDocument.Load(stream);
+                var xNews = xdoc.Root.Descendants("title");
+                foreach (var data in xNews) {
+                    Console.WriteLine(data);
+                }
+            }
+        }
+    }
+}
