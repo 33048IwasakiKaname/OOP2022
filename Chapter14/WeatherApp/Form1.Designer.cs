014f8a20b98a948a5a4f0b76cc831782c83d3da9
33048岩崎要 <33048@ojs.ac.jp>
2022-11-10 15:58:11 +0900

    途中

---
+++ Chapter14/WeatherApp/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿
+namespace WeatherApp {
+    partial class Form1 {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent() {
+            this.btWeatherGet = new System.Windows.Forms.Button();
+            this.tbWeatherInfo = new System.Windows.Forms.TextBox();
+            this.cbRegion = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btWeatherGet
+            // 
+            this.btWeatherGet.Location = new System.Drawing.Point(394, 12);
+            this.btWeatherGet.Name = "btWeatherGet";
+            this.btWeatherGet.Size = new System.Drawing.Size(159, 65);
+            this.btWeatherGet.TabIndex = 0;
+            this.btWeatherGet.Text = "取得";
+            this.btWeatherGet.UseVisualStyleBackColor = true;
+            this.btWeatherGet.Click += new System.EventHandler(this.btWeatherGet_Click);
+            // 
+            // tbWeatherInfo
+            // 
+            this.tbWeatherInfo.Location = new System.Drawing.Point(221, 216);
+            this.tbWeatherInfo.Multiline = true;
+            this.tbWeatherInfo.Name = "tbWeatherInfo";
+            this.tbWeatherInfo.Size = new System.Drawing.Size(332, 109);
+            this.tbWeatherInfo.TabIndex = 1;
+            // 
+            // cbRegion
+            // 
+            this.cbRegion.FormattingEnabled = true;
+            this.cbRegion.Items.AddRange(new object[] {
+            "青森県",
+            "岩手県",
+            "宮城県",
+            "秋田県",
+            "山形県",
+            "福島県",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "",
+            ""});
+            this.cbRegion.Location = new System.Drawing.Point(221, 138);
+            this.cbRegion.Name = "cbRegion";
+            this.cbRegion.Size = new System.Drawing.Size(157, 20);
+            this.cbRegion.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(23, 123);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(173, 35);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "地域選択：";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(563, 346);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbRegion);
+            this.Controls.Add(this.tbWeatherInfo);
+            this.Controls.Add(this.btWeatherGet);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btWeatherGet;
+        private System.Windows.Forms.TextBox tbWeatherInfo;
+        private System.Windows.Forms.ComboBox cbRegion;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Chapter14/WeatherApp/Form1.cs
@@ -0,0 +1,47 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WeatherApp {
+
+    public partial class Form1 : Form {
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void btWeatherGet_Click(object sender, EventArgs e) {
+            var wc = new WebClient() {
+                Encoding = Encoding.UTF8
+            };
+
+            var num = 100000;
+
+            var dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/" + num + ".json");
+
+            var json = JsonConvert.DeserializeObject<Rootobject>(dString);
+
+            tbWeatherInfo.Text = json.text;
+        }
+
+
+        private static void Form1_Load(object sender, EventArgs e) {
+            var regionDict = new Dictionary<string, int>() {
+                {"青森県", 20000},
+                {"岩手県", 30000},
+                {"宮城県", 40000},
+                {"秋田県", 50000},
+                {"山形県", 60000},
+                {"福島県", 70000},
+            };
+        }
+    }
+}
