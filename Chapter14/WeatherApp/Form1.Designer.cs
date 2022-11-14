2af1e37b5be358f2536c2b515c13ff734f331c31
33048岩崎要 <33048@ojs.ac.jp>
2022-11-14 13:56:21 +0900

    47都道府県の天気表示完成

--- Chapter14/WeatherApp/Form1.Designer.cs
+++ Chapter14/WeatherApp/Form1.Designer.cs
@@ -42,40 +42,64 @@ namespace WeatherApp {
             // 
             // tbWeatherInfo
             // 
-            this.tbWeatherInfo.Location = new System.Drawing.Point(221, 216);
+            this.tbWeatherInfo.Location = new System.Drawing.Point(208, 165);
             this.tbWeatherInfo.Multiline = true;
             this.tbWeatherInfo.Name = "tbWeatherInfo";
-            this.tbWeatherInfo.Size = new System.Drawing.Size(332, 109);
+            this.tbWeatherInfo.Size = new System.Drawing.Size(343, 169);
             this.tbWeatherInfo.TabIndex = 1;
             // 
             // cbRegion
             // 
             this.cbRegion.FormattingEnabled = true;
             this.cbRegion.Items.AddRange(new object[] {
+            "北海道(札幌周辺)",
             "青森県",
             "岩手県",
             "宮城県",
             "秋田県",
             "山形県",
             "福島県",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            "",
-            ""});
-            this.cbRegion.Location = new System.Drawing.Point(221, 138);
+            "茨城県",
+            "栃木県",
+            "群馬県",
+            "埼玉県",
+            "千葉県",
+            "東京都",
+            "神奈川県",
+            "山梨県",
+            "長野県",
+            "岐阜県",
+            "静岡県",
+            "愛知県",
+            "三重県",
+            "新潟県",
+            "富山県",
+            "石川県",
+            "福井県",
+            "滋賀県",
+            "京都府",
+            "大阪府",
+            "兵庫県",
+            "奈良県",
+            "和歌山県",
+            "鳥取県",
+            "島根県",
+            "岡山県",
+            "広島県",
+            "徳島県",
+            "香川県",
+            "愛媛県",
+            "高知県",
+            "山口県",
+            "福岡県",
+            "佐賀県",
+            "長崎県",
+            "熊本県",
+            "大分県",
+            "宮崎県",
+            "鹿児島県",
+            "沖縄本島"});
+            this.cbRegion.Location = new System.Drawing.Point(221, 99);
             this.cbRegion.Name = "cbRegion";
             this.cbRegion.Size = new System.Drawing.Size(157, 20);
             this.cbRegion.TabIndex = 2;
@@ -84,7 +108,7 @@ namespace WeatherApp {
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("MS UI Gothic", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label1.Location = new System.Drawing.Point(23, 123);
+            this.label1.Location = new System.Drawing.Point(12, 99);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(173, 35);
             this.label1.TabIndex = 3;
@@ -101,7 +125,6 @@ namespace WeatherApp {
             this.Controls.Add(this.btWeatherGet);
             this.Name = "Form1";
             this.Text = "Form1";
-            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
--- Chapter14/WeatherApp/Form1.cs
+++ Chapter14/WeatherApp/Form1.cs
@@ -14,34 +14,78 @@ namespace WeatherApp {
 
     public partial class Form1 : Form {
 
-        public Form1() {
-            InitializeComponent();
-        }
-
-        private void btWeatherGet_Click(object sender, EventArgs e) {
-            var wc = new WebClient() {
-                Encoding = Encoding.UTF8
-            };
-
-            var num = 100000;
-
-            var dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/" + num + ".json");
-
-            var json = JsonConvert.DeserializeObject<Rootobject>(dString);
-
-            tbWeatherInfo.Text = json.text;
-        }
-
-
-        private static void Form1_Load(object sender, EventArgs e) {
-            var regionDict = new Dictionary<string, int>() {
+        private Dictionary<string, int> regionDict = new Dictionary<string, int>() {
+                {"北海道(札幌周辺)", 16000},
                 {"青森県", 20000},
                 {"岩手県", 30000},
                 {"宮城県", 40000},
                 {"秋田県", 50000},
                 {"山形県", 60000},
                 {"福島県", 70000},
+                {"茨城県", 80000},
+                {"栃木県", 90000},
+                {"群馬県", 100000},
+                {"埼玉県", 110000},
+                {"千葉県", 120000},
+                {"東京都", 130000},
+                {"神奈川県", 140000},
+                {"山梨県", 190000},
+                {"長野県", 200000},
+                {"岐阜県", 210000},
+                {"静岡県", 220000},
+                {"愛知県", 230000},
+                {"三重県", 240000},
+                {"新潟県", 150000},
+                {"富山県", 160000},
+                {"石川県", 170000},
+                {"福井県", 180000},
+                {"滋賀県", 250000},
+                {"京都府", 260000},
+                {"大阪府", 270000},
+                {"兵庫県", 280000},
+                {"奈良県", 290000},
+                {"和歌山県", 300000},
+                {"鳥取県", 310000},
+                {"島根県", 320000},
+                {"岡山県", 330000},
+                {"広島県", 340000},
+                {"徳島県", 360000},
+                {"香川県", 370000},
+                {"愛媛県", 380000},
+                {"高知県", 390000},
+                {"山口県", 350000},
+                {"福岡県", 400000},
+                {"佐賀県", 410000},
+                {"長崎県", 420000},
+                {"熊本県", 430000},
+                {"大分県", 440000},
+                {"宮崎県", 450000},
+                {"鹿児島県", 460100},
+                {"沖縄本島地方", 471000},
             };
+
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void btWeatherGet_Click(object sender, EventArgs e) {
+
+            tbWeatherInfo.ResetText();
+
+            var wc = new WebClient() {
+                Encoding = Encoding.UTF8
+            };
+
+
+            var value = regionDict.FirstOrDefault(x => x.Key == cbRegion.SelectedItem.ToString()).Value;
+            var s = string.Format("{0:000000}", value);
+            
+
+            var dString = wc.DownloadString("https://www.jma.go.jp/bosai/forecast/data/overview_forecast/" + s + ".json");
+
+            var json = JsonConvert.DeserializeObject<Rootobject>(dString);
+
+            tbWeatherInfo.Text = json.text;
         }
     }
 }
